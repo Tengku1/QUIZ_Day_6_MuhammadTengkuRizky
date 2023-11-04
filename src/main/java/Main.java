9be53fc6ed5c48c597599822b637fa3bfde01119
Tengku1 <anountengku@gmail.com>
2023-11-04 09:56:27 +0700

    optimized program, and add do while at main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,68 +3,88 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Aplikasi Catatan Kendaraan");
-        System.out.println("1. Lihat Daftar Kendaraan\n2. Lihat Total Pendapatan");
-        System.out.print("Pilih Menu : ");
-        int firstOption = scanner.nextInt();
-        System.out.println();
-        scanner.nextLine();
-        System.out.println("=======================================================");
         VehicleInterface vehicleInterface = new VehicleImplementation();
-        if(firstOption == 1) {
-            System.out.println("Pilih Jenis Kendaraan : ");
-            System.out.println("1. SUV\n2. Taxi\n3. Private Jet\n4. Boat\n5. All Vehicles");
-            System.out.print("Pilih Menu : ");
-            int secondOption = scanner.nextInt();
-            System.out.println();
+        Summary summary = new Summary();
+        String response;
 
-            if(secondOption == 1) {
-                var listVehicles = vehicleInterface.dataSuv();
-                vehicleInterface.printVehicles(listVehicles);
-            }
-            if(secondOption == 2) {
-                var listVehicles = vehicleInterface.dataTaxi();
-                vehicleInterface.printVehicles(listVehicles);
-            }
-            if(secondOption == 3) {
-                var listVehicles = vehicleInterface.dataPrivateJet();
-                vehicleInterface.printVehicles(listVehicles);
-            }
-            if(secondOption == 4) {
-                var listVehicles = vehicleInterface.dataBoat();
-                vehicleInterface.printVehicles(listVehicles);
-            }
-            if(secondOption == 5) {
-                var listVehicles = vehicleInterface.AllVehicles();
-                vehicleInterface.printVehicles(listVehicles);
-            }
-        }
-        if(firstOption == 2) {
-            System.out.println("Pilih Jenis Kendaraan : ");
-            System.out.println("1. Cars\n2. Planes\n3. Boats\n4. All Vehicles");
+        do {
+            System.out.println("Aplikasi Catatan Kendaraan");
+            System.out.println("1. Lihat Daftar Kendaraan\n2. Lihat Total Pendapatan");
             System.out.print("Pilih Menu : ");
-            int secondOption = scanner.nextInt();
-            System.out.println();
-            Summary summary = new Summary();
+            int firstOption = scanner.nextInt();
 
-            switch (secondOption) {
+            switch (firstOption) {
                 case 1:
-                    long incomeCars = summary.incomeCars();
-                    summary.printTotalIncome("TotalIncomeCar ",incomeCars);
+                    System.out.println("Pilih Jenis Kendaraan : ");
+                    System.out.println("1. SUV\n2. Taxi\n3. Private Jet\n4. Boat\n5. All Vehicles");
+                    System.out.print("Pilih Menu : ");
+                    int secondOption = scanner.nextInt();
+
+                    switch (secondOption) {
+                        case 1:
+                            var listSuv = vehicleInterface.dataSuv();
+                            vehicleInterface.printVehicles(listSuv);
+                            break;
+                        case 2:
+                            var listTaxi = vehicleInterface.dataTaxi();
+                            vehicleInterface.printVehicles(listTaxi);
+                            break;
+                        case 3:
+                            var listPlanes = vehicleInterface.dataPrivateJet();
+                            vehicleInterface.printVehicles(listPlanes);
+                            break;
+                        case 4:
+                            var listBoat = vehicleInterface.dataBoat();
+                            vehicleInterface.printVehicles(listBoat);
+                            break;
+                        case 5:
+                            var listVehicles = vehicleInterface.AllVehicles();
+                            vehicleInterface.printVehicles(listVehicles);
+                            break;
+                        default:
+                            System.out.println("Pilihan Tidak Valid !");
+                            break;
+                    }
                     break;
                 case 2:
-                    long incomePlanes = summary.incomePlanes();
-                    summary.printTotalIncome("TotalIncomePlane ",incomePlanes);
+                    System.out.println("Pilih Jenis Kendaraan : ");
+                    System.out.println("1. Cars\n2. Planes\n3. Boats\n4. All Vehicles");
+                    System.out.print("Pilih Menu : ");
+                    int secondOptionIncome = scanner.nextInt();
+
+                    switch (secondOptionIncome) {
+                        case 1:
+                            long incomeCars = summary.incomeCars();
+                            summary.printTotalIncome("TotalIncomeCar ", incomeCars);
+                            break;
+                        case 2:
+                            long incomePlanes = summary.incomePlanes();
+                            summary.printTotalIncome("TotalIncomePlane ", incomePlanes);
+                            break;
+                        case 3:
+                            long incomeBoats = summary.incomeBoats();
+                            summary.printTotalIncome("TotalIncomeBoat ", incomeBoats);
+                            break;
+                        case 4:
+                            long totalIncome = summary.incomeCars() + summary.incomePlanes() + summary.incomeBoats();
+                            summary.printTotalIncome("All Total Vehicle Incomes ", totalIncome);
+                            break;
+                        default:
+                            System.out.println("Pilihan Tidak Valid !");
+                            break;
+                    }
                     break;
-                case 3:
-                    long incomeBoats = summary.incomeBoats();
-                    summary.printTotalIncome("TotalIncomeBoat ",incomeBoats);
-                    break;
-                case 4:
-                    summary.printTotalIncome("All Total Vehicle Incomes ",summary.incomeCars() + summary.incomePlanes() + summary.incomeBoats());
+                default:
+                    System.out.println("Pilihan Tidak Valid !");
                     break;
             }
-        }
-        System.out.println("=======================================================");
+
+            System.out.print("Apakah Anda ingin melihat menu lagi? (y/n): ");
+            response = scanner.next();
+            System.out.println();
+        } while (response.equalsIgnoreCase("y"));
+
+        System.out.println("Terima kasih telah menggunakan aplikasi Catatan Kendaraan. Selamat tinggal!");
+        scanner.close();
     }
 }
